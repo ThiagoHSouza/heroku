e566c0f4f6d94e49eeaeba77dbc4ffe72a93546e
de Souza <tgsa@gft.com>
2017-11-13 11:07:49 -0200

    first commit

---
+++ src/main/java/br/com/developer/openshift/TesteOpenshift1Application.java
@@ -0,0 +1,20 @@
+package br.com.developer.openshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class TesteOpenshift1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteOpenshift1Application.class, args);
+	}
+	
+	@GetMapping("/")
+	public String teste(){
+		return "HelloWorld";
+	}
+}
