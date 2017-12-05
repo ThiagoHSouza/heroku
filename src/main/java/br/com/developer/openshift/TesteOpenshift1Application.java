536651729ad9c350dc08a7b2ec309ec45eafbe12
tgsa <tgsa@PCCTA031.gft.com>
2017-12-05 16:41:28 -0200

    cacheable

--- src/main/java/br/com/developer/openshift/TesteOpenshift1Application.java
+++ src/main/java/br/com/developer/openshift/TesteOpenshift1Application.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,7 @@ public class TesteOpenshift1Application {
 	private static Integer count = 0;
 	
 	@GetMapping("redis/{id}")
+	@Cacheable("count")
 	public String testeRedis(@PathVariable("id") String id){
 		return "Redis : " + id + " : CACHE : " + (count++);
 	}
