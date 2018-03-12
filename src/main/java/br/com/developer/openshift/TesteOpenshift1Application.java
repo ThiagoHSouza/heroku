5f54478267a6eea59d2f5799b8c5be02189da943
ThiagoHSouza <thiago_souza07@hotmail.com>
2018-03-12 10:55:58 -0300

    Update TesteOpenshift1Application.java

--- src/main/java/br/com/developer/openshift/TesteOpenshift1Application.java
+++ src/main/java/br/com/developer/openshift/TesteOpenshift1Application.java
@@ -30,7 +30,7 @@ public class TesteOpenshift1Application {
 	}
 	
 	@GetMapping("save/{text}")
-	public Pessoa save((@PathVariable("text") String text){
+	public Pessoa save(@PathVariable("text") String text){
 		return rep.save(new Pessoa(text)); 
 	}
 	
